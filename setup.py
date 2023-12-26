de4beb5f067dc9f8856bde78198305c03acd2312
Kaushik Chemburkar <attherate@gmail.com>
2023-12-27 10:31:06 +1100

    Added comments setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python
 
+# Importing necessary functions from setuptools package.
 from setuptools import setup
+# Importing path manipulation utilities.
 from os.path import dirname, abspath, join
 
+# Setting the path to the README file which is used as the long description for the package.
 long_description_path = join(dirname(abspath(__file__)), 'README.md')
+# Reading the content of README.md to use as the long description.
 long_description = open(long_description_path, encoding='utf-8').read()
 
+# Calling the setup function to handle the packaging of the module.
 setup(
-    name='pretty-csv-diff',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/telnet23/pretty-csv-diff',
-    license='Apache License 2.0',
-    use_scm_version=True,
+    name='pretty-csv-diff',  # Name of the package.
+    long_description=long_description,  # Long description read from README.md.
+    long_description_content_type='text/markdown',  # Specifying the format of the long description.
+    url='https://github.com/telnet23/pretty-csv-diff',  # URL of the project's homepage.
+    license='Apache License 2.0',  # License type for the package.
+    use_scm_version=True,  # Enabling the use of setuptools_scm for versioning.
     setup_requires=[
-        'setuptools_scm',
+        'setuptools_scm',  # Dependency required for setuptools_scm to work.
     ],
     packages=[
-        'pretty_csv_diff',
+        'pretty_csv_diff',  # List of all packages that are part of this distribution.
     ],
     entry_points={
         'console_scripts': [
-            'pretty-csv-diff = pretty_csv_diff.__main__:main',
+            'pretty-csv-diff = pretty_csv_diff.__main__:main',  # Entry point for the console script.
         ],
     },
     classifiers=[
+        # Metadata classifiers to categorize the project.
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
